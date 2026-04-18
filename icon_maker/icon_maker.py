96440fa8ac9087ac4eb803ba980e4766de0a1841
Mateusz Machowina <133600829+MateuszMachowina@users.noreply.github.com>
2026-04-18 21:38:19 +0200

    Add files via upload

---
+++ icon_maker/icon_maker.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+# 1. Wczytaj swój oryginalny plik PNG
+img = Image.open('ikonka.png') # <--- Zmień na nazwę swojego obrazka
+
+# 2. Pobierz jego wymiary i znajdź dłuższy bok
+szerokosc, wysokosc = img.size
+max_bok = max(szerokosc, wysokosc)
+
+# 3. Stwórz nowy, całkowicie przezroczysty obraz, który jest idealnym kwadratem
+tlo = Image.new('RGBA', (max_bok, max_bok), (255, 255, 255, 0))
+
+# 4. Oblicz środek i wklej na niego swój czołg
+offset = ((max_bok - szerokosc) // 2, (max_bok - wysokosc) // 2)
+tlo.paste(img, offset)
+
+# 5. Zapisz gotowy kwadrat do formatu ICO ze wszystkimi rozmiarami
+tlo.save(
+    'icon.ico', 
+    format='ICO', 
+    sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)]
+)
+
+print("Sukces! Stworzono idealnie kwadratową i ostrą ikonę.")
